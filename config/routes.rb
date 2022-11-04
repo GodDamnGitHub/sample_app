c558e4018836e18851862190515c4f489fc557cd
GodDamnGitHub <39892107+GodDamnGitHub@users.noreply.github.com>
2022-11-04 01:15:12 -0400

    13.3.3

--- config/routes.rb
+++ config/routes.rb
@@ -18,5 +18,6 @@ Rails.application.routes.draw do
   root "static_pages#home"
   resources :users
   resources :microposts,          only: [:create, :destroy]
+  get '/microposts', to: 'static_pages#home'
 
 end
